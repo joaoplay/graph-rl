0c3048a99b41d3367cfe16e82b41b8dd99b3feb8
João Brás Simões <joaosbraz@gmail.com>
2022-02-26 21:22:22 +0000

    Deactivate Neptune logging

--- neptune_logging.py
+++ neptune_logging.py
@@ -1,35 +1,42 @@
-from neptune.new.types import File
+# from neptune.new.types import File
 
-from settings import NEPTUNE_INSTANCE
+# from settings import NEPTUNE_INSTANCE
 
 
 def log_batch_training_result(loss, reward):
+    return
     NEPTUNE_INSTANCE['training/batch/loss'].log(loss)
     NEPTUNE_INSTANCE['training/batch/reward'].log(reward)
 
 
 def log_batch_validation_result(performance):
+    return
     NEPTUNE_INSTANCE['validation/batch/peformance'].log(performance)
 
 
 def log_training_simulation_results(average_reward):
+    return
     NEPTUNE_INSTANCE['training/simulation/average_reward'].log(average_reward)
 
 
 def log_validation_simulation_results(average_reward):
+    return
     NEPTUNE_INSTANCE['validation/simulation/average_reward'].log(average_reward)
 
 
 def upload_graph_plot(plot, iteration):
+    return
     NEPTUNE_INSTANCE[f'validation/visualization/graphs/training-step-{iteration}.jpeg'].upload(File.as_image(plot))
 
 
 def upload_action_frequency(plot, iteration, graph_idx):
+    return
     NEPTUNE_INSTANCE[f'validation/visualization/action-frequency/hist-it{iteration}-graph{graph_idx}.jpeg'].upload(
         File.as_image(plot))
 
 
 def upload_irrigation_heatmaps(sources_heatmap, irrigation_heatmap, iteration, stage):
+    return
     NEPTUNE_INSTANCE[f'{stage}/visualization/irrigation_heatmap/sources-heatmap-{iteration}.jpeg'].upload(
         File.as_image(sources_heatmap))
     NEPTUNE_INSTANCE[f'{stage}/visualization/irrigation_heatmap/irrigation-heatmap-{iteration}.jpeg'].upload(
