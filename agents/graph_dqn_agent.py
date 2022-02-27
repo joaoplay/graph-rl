91f7bf103c9c220ca362ac675b355e99aa69b621
João Brás Simões <joaosbraz@gmail.com>
2022-02-27 21:22:24 +0000

    Restore episodic reward

--- agents/graph_dqn_agent.py
+++ agents/graph_dqn_agent.py
@@ -380,7 +380,7 @@ class GraphDQNAgent(BaseAgent):
             self.environment.step(actions)
 
             # Calculate reward
-            rewards = self.environment.calculate_reward_all_graphs()
+            rewards = np.zeros(len(self.environment.graphs_list)) #self.environment.calculate_reward_all_graphs()
 
             graphs_states = [(graph, graph.selected_start_node, graph.forbidden_actions) for graph in self.environment.graphs_list]
 
