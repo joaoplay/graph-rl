e3128635cb6f2e7d0a61fbae5abcc0e60cc9de22
João Brás Simões <joaosbraz@gmail.com>
2022-02-28 02:35:17 +0000

    .

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 import os
 
-#import neptune.new as neptune
+import neptune.new as neptune
 
-"""NEPTUNE_INSTANCE = neptune.init(project="jbsimoes/graph-rl",
+NEPTUNE_INSTANCE = neptune.init(project="jbsimoes/graph-rl",
                                 api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjYmQ4MjE1OC0yNzBhLTQyNzctYjFmZS00YTFhYjYxZTdmMjUifQ==",
                                 source_files=['*.py'],
-                                mode=os.getenv("NEPTUNE_MODE", "async"))"""
+                                mode=os.getenv("NEPTUNE_MODE", "async"))
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
