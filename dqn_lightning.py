cfddfa8bef42f077aab7322d6ef8518d9beab9af
João Brás Simões <joaosbraz@gmail.com>
2022-04-08 18:43:41 +0100

    .

--- dqn_lightning.py
+++ dqn_lightning.py
@@ -25,7 +25,7 @@ class DQNLightning(LightningModule):
 
     def __init__(self, env: GraphEnv = None, graphs=None, batch_size: int = 64, hidden_size: int = 28, lr: float = 1e-4,
                  gamma: float = 0.99, sync_rate: int = 10000, replay_size: int = 10 ** 6, warm_start_size: int = 100000,
-                 eps_last_frame: int = 10 ** 10, eps_start: float = 1.0, eps_end: float = 0.0, episode_length: int = 200,
+                 eps_last_frame: int = 10 ** 5, eps_start: float = 1.0, eps_end: float = 0.0, episode_length: int = 200,
                  warm_start_steps: int = 50000, action_modes: tuple[int] = DEFAULT_ACTION_MODES) -> None:
         super().__init__()
 
