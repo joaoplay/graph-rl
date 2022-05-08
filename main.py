bc22df7ed7f73c3afd97d49a91516e34a1b598ca
João Brás Simões <joaosbraz@gmail.com>
2022-05-08 22:38:36 +0100

    Add PATH

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ from settings import USE_CUDA
 
 os.environ["WANDB_API_KEY"] = '237099249b3c0e91437061c393ab089d03339bc3'
 
-wandb.init(project="graph-rl", entity="jbsimoes", mode=os.getenv("WANDB_UPLOAD_MODE", "online"))
+# FIXME: Move it to ENV variable. I can't do it now because I don't want to deal with the DOCKER rebuild process.
+WANDB_PATH = '/media/storage/jbsimoes' if USE_CUDA else '.'
+
+wandb.init(project="graph-rl", entity="jbsimoes", mode=os.getenv("WANDB_UPLOAD_MODE", "online"), dir=WANDB_PATH)
 
 
 @hydra.main(config_path="configs", config_name="default_config")
