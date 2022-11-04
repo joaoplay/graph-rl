879c59b5340be362cbd77a181aea4ceaa3aac391
João Braz Simões <joaosbraz@gmail.com>
2022-11-04 19:01:03 +0000

    .

--- settings.py
+++ settings.py
@@ -5,7 +5,8 @@ import neptune.new as neptune
 NEPTUNE_INSTANCE = neptune.init(project="jbsimoes/graph-rl",
                                 api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjYmQ4MjE1OC0yNzBhLTQyNzctYjFmZS00YTFhYjYxZTdmMjUifQ==",
                                 source_files=['*.py'],
-                                mode=os.getenv("NEPTUNE_MODE", "async"))
+                                mode=os.getenv("NEPTUNE_MODE", "async"),
+                                name=os.getenv("NEPTUNE_RUN_NAME", None))
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
