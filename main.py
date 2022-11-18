9ca1f54359174666eb70aa16bafffafe86ec933a
João Braz Simões <joaosbraz@gmail.com>
2022-11-18 16:03:40 +0000

    .

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def run_experiment(cfg: DictConfig):
                            irrigation_grid_cell_size=cfg.irrigation_grid_cell_size)
     train_graphs = graph_generator.generate_multiple_graphs(cfg.number_of_graphs)
 
-    model = DQN(env=environmcent, graphs=train_graphs, num_dataloader_workers=cfg.num_dataloader_workers,
+    model = DQN(env=environment, graphs=train_graphs, num_dataloader_workers=cfg.num_dataloader_workers,
                 multi_action_q_network=cfg.multi_action_q_network, **cfg.core, device='cuda')
     model.populate(model.hparams.warm_start_steps)
 
