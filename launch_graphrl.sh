341f114e44203386d2e65a5156f45b88ee5c97f3
João Braz Simões <joaosbraz@gmail.com>
2023-11-13 02:40:07 +0000

    .

--- launch_graphrl.sh
+++ launch_graphrl.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1 # Ensure that all cores are on one machine
 #SBATCH -t 2-00:00 # Runtime in D-HH:MM
 #SBATCH -p cpu2 # Partition to submit to
-#SBATCH --mem=3000 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=7000 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o log.out # File to which STDOUT will be written
 #SBATCH -e log.err # File to which STDERR will be written
 #SBATCH --mail-type=ALL # Type of email notification- BEGIN,END,FAIL,ALL
