16ae8e7a2027e3f97fff652da4d6371867a23ab7
João Braz Simões <joaosbraz@gmail.com>
2023-12-17 17:53:41 +0000

    Update maximum execution time

--- launch_graphrl.sh
+++ launch_graphrl.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #SBATCH -A vascnetgen # Project name
-#SBATCH -n 200 # Number of cores
-#SBATCH -N 3 # Ensure that all cores are on one machine
+#SBATCH -n 1000 # Number of cores
 #SBATCH -t 2-00:00 # Runtime in D-HH:MM
 #SBATCH -p cpu2 # Partition to submit to
 #SBATCH --mem=7000 # Memory pool for all cores (see also --mem-per-cpu)
