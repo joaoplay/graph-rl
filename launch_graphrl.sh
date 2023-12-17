e39ef0dfdfa0501c48198f27cc10b7bceca0550e
João Braz Simões <joaosbraz@gmail.com>
2023-12-17 18:07:23 +0000

    .

--- launch_graphrl.sh
+++ launch_graphrl.sh
@@ -3,6 +3,7 @@
 #SBATCH -A vascnetgen # Project name
 #SBATCH -n 300 # Number of cores
 #SBATCH -t 2-00:00 # Runtime in D-HH:MM
+#SBATCH -p gpu # Partition to submit to
 #SBATCH --mem=7000 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o log.out # File to which STDOUT will be written
 #SBATCH -e log.err # File to which STDERR will be written
