ac495bbda028dc795eb6818588c5efac9eb9406c
João Braz Simões <joaosbraz@gmail.com>
2023-12-17 18:24:59 +0000

    .

--- launch_graphrl.sh
+++ launch_graphrl.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH -A vascnetgen # Project name
-#SBATCH -n 100 # Number of cores
 #SBATCH -t 2-00:00 # Runtime in D-HH:MM
-#SBATCH -p gpu,cpu1,cpu2 # Partition to submit to
 #SBATCH --mem=7000 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o log.out # File to which STDOUT will be written
 #SBATCH -e log.err # File to which STDERR will be written
 #SBATCH --mail-type=ALL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=jbsimoes@dei.uc.pt # Email to which notifications will be sent
-#SBATCH --gres=gpu:1 # Number of GPUs per node
+#SBATCH --partition=gpu    # Specify the partition name
+#SBATCH --gres=gpu:1       # Number of GPUs
+#SBATCH --cpus-per-task=100 # Number of CPUs per task
 
 # Run the program and pass the arguments from the command line
 poetry run python /veracruz/projects/v/vascnetgen/graph-rl/main.py
